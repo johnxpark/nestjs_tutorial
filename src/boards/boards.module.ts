0981e2d72530ccd4565fd87918a380ad99d19ee3
johnxpark <pyh9049@gmail.com>
2021-11-15 21:08:37 +0900

    feat: add boards.entity.ts and boards.repository.ts

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsController } from './boards.controller';
+import { BoardsRepository } from './boards.repository';
 import { BoardsService } from './boards.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([BoardsRepository])],
   controllers: [BoardsController],
-  providers: [BoardsService]
+  providers: [BoardsService],
 })
 export class BoardsModule {}

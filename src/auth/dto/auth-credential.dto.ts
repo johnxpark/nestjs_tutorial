2a39521784a45972b986bd34bdbe3fa846335625
johnxpark <pyh9049@gmail.com>
2021-11-17 22:44:13 +0900

    feat: Apply ValidationPipe when signing in auth module

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -1,4 +1,19 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/^[a-zA-Z0-9]*$/, {
+    message: 'Only English and numbers are allowed for ID.',
+  })
   username: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/^[a-zA-Z0-9]*$/, {
+    message: 'Only English and numbers are allowed for password.',
+  })
   password: string;
 }
